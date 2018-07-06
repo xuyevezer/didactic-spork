0049c41d514a6d1fe228a093d18fe124fd339854
Sedetius <bla@blub.de>
2018-07-06 20:30:04 +0200

    Wrote authenticated decryption for recieving packages.

--- BankingServer/src/Utility.java
+++ BankingServer/src/Utility.java
@@ -1,16 +1,15 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.security.InvalidKeyException;
 import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
 
 import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Helper class containing auxiliary functions.
@@ -57,6 +56,11 @@ public class Utility
 	 */
 	private static String initVec;
 	
+	/**
+	 * List of Countervalues of ingoing messages
+	 */
+	private static List<Integer> msgInCounter = new ArrayList<>();
+	
     /**
      * Controls whether we are in lab or testing mode. Lab mode enables
      * communication features like sending mails and tokens.
@@ -88,6 +92,7 @@ public class Utility
      */
     public static void sendPacket(DataOutputStream outputStream, String payload) throws IOException
     {
+    	// Make a new MAC generator
     	try {
 			mac = Mac.getInstance("HmacSHA256");
 			mac.init(macKey);
@@ -95,13 +100,23 @@ public class Utility
 			e.printStackTrace();
 		}
     	
+    	// Get a new Timestamp
     	Timestamp timestamp = new Timestamp(calendar.getTimeInMillis());
     	
-    	//payload + timestamp + counter
+    	// Assemble data
+    	// payload + timestamp + counter
     	String data = payload + "::" + timestamp.getTime() + "::" + msgOutCounter;
     	
+    	// Increase Counter for outgoing messages, so it will not be use again
+    	if(msgOutCounter + 1 >= Integer.MAX_VALUE) {
+    		msgOutCounter = 0;
+    	}
+    	msgOutCounter ++;
+    	
+    	// Encrypt data
     	String cipher = Encryptor.encrypt(encKey, initVec, data);
     	
+    	// Authenticate cipher
     	mac.update(cipher.getBytes());
     	String hmac = Base64.getEncoder().encodeToString(mac.doFinal());
     	
@@ -128,9 +143,81 @@ public class Utility
         // Prepare payload buffer
         byte[] payloadEncoded = new byte[inputStream.readInt()];
         inputStream.readFully(payloadEncoded);
-
+        
+        String payLoad = new String(payloadEncoded);
+        
+        // Split package in cipher and HMAC
+        String[] split = new String[2];
+        String rcvCipher = "";
+        String rcvHmac = "";
+        
+        try {
+            split = payLoad.split("::");
+            rcvCipher = split[0];
+            rcvHmac = split[1];
+        } catch(Exception e) {
+        	e.printStackTrace();
+        	return "Error: Bad Package.";
+        }
+        
+        // Check HMAC
+        // Make a new MAC generator
+    	try {
+			mac = Mac.getInstance("HmacSHA256");
+			mac.init(macKey);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	// Authenticate cipher
+    	mac.update(rcvCipher.getBytes());
+    	String hmac = Base64.getEncoder().encodeToString(mac.doFinal());
+    	
+    	// If hmac is diffrent from the recieved one
+    	if(hmac != rcvHmac) {
+    		// Someone tamperd with the package
+    		// Error
+    		return "Error: Bad HMAC.";
+    	}
+    	
+    	// Decrypt
+    	String data = Encryptor.decrypt(encKey, initVec, rcvCipher);
+    	
+    	// Dissassamble data
+    	try {
+        	split = data.split("::");
+    	} catch (Exception e) {
+    		
+    	}
+    	
+    	String payload = split [0];
+    	Timestamp timestamp = new Timestamp(Long.parseLong(split [1]));
+    	int msgCounter = Integer.parseInt(split [2]);
+    	
+    	if(timestamp.compareTo(calendar.getTime()) < 0) {
+    		long diffrence = calendar.getTimeInMillis() - timestamp.getTime();
+    		if(diffrence < 300000) {
+    			// TODO: Close Input Stream
+    			return "Error: Recieved Package is too old.";
+    		}
+    	}
+    	
+    	// Check if messageCounter was already used
+    	if(msgInCounter.contains(msgCounter)) {
+    		return "Error: Package counter already used.";
+    	}
+    	
+    	// Check if msgCounter might overflow
+    	if(msgCounter + 1 >= Integer.MAX_VALUE) {
+    		msgInCounter.clear();
+    	}
+        
+    	// Add messageCounter to list
+    	msgInCounter.add(msgCounter);
+    	
+        
         // Decode payload
-        return new String(payloadEncoded);
+        return payload;
     }
 
     /**
